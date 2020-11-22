00994d79ed99a9f326d6dcc739457595ac3df909
wasabigeek <wasabigeek@users.noreply.github.com>
2020-11-22 16:17:39 +0800

    Clean up icon configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,13 +66,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `wasabigeek`,
+        short_name: `wasabigeek`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/wasabigeek-logo.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
