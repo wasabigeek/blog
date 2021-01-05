f00a20e9086835a45516e6a572b95ec3d23ecd39
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-01-05 08:43:57 +0800

    feat: Tweak typography further

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,29 @@
 import Typography from "typography"
 import Kirkham from "typography-theme-kirkham"
 
-
 delete Kirkham.googleFonts
+Kirkham.overrideThemeStyles = ({ rhythm }, options, styles) => ({
+  'h2,h3': {
+    marginBottom: rhythm(1 / 2),
+    marginTop: rhythm(2),
+  },
+  'table': {
+    fontSize: '0.9rem',
+    border: '1px solid hsla(0,0%,0%,0.12)',
+    borderRadius: '5px'
+  },
+  'th': {
+    backgroundColor: '#eef0f159',
+  },
+  'th, td': {
+    paddingTop: '0.5rem',
+    paddingBottom: 'calc(0.5rem - 1px)',
+  },
+  'th:first-child, td:first-child': {
+    paddingLeft: '0.96rem'
+  },
+})
+
 
 const typography = new Typography(Kirkham)
 
