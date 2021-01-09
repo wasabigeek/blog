2a4c935ca414abf7833d8b60eca75f50533a39f0
wasabigeek <wasabigeek@users.noreply.github.com>
2021-01-09 13:41:02 +0800

    Add more wasabi questions

--- src/components/wasabi_quiz.js
+++ src/components/wasabi_quiz.js
@@ -2,10 +2,9 @@ import React from "react"
 
 import { rhythm } from "../utils/typography"
 
-const WasabiQuiz = ({ }) => {
-  return (
-    <section style={{ marginBottom: rhythm(2) }}>
-      <h2>Random Wasabi Question</h2>
+const WasabiQuiz = () => {
+  const questions = [
+    <React.Fragment>
       <p>
         Which plant belongs to the same family as wasabi?
       </p>
@@ -15,6 +14,47 @@ const WasabiQuiz = ({ }) => {
         <li>Potato</li>
         <li>Onion</li>
       </ol>
+    </React.Fragment>,
+    <React.Fragment>
+      <p>
+        How long does it take for wasabi to mature?
+      </p>
+      <ol>
+        <li>6 months</li>
+        <li>8 months</li>
+        <li>1 year</li>
+        <li>2 years</li>
+      </ol>
+    </React.Fragment>,
+    <React.Fragment>
+      <p>
+        What is the main component of wasabi substitutes?
+      </p>
+      <ol>
+        <li>Mustard</li>
+        <li>Horseradish</li>
+        <li>Green Chilli</li>
+        <li>White Peppercorns</li>
+      </ol>
+    </React.Fragment>,
+    <React.Fragment>
+      <p>
+        Which part of the wasabi plant is used to create the sushi condiment?
+      </p>
+      <ol>
+        <li>Stem</li>
+        <li>Root</li>
+        <li>Leaves</li>
+        <li>Fruit</li>
+      </ol>
+    </React.Fragment>,
+  ]
+  const choice = Math.floor(Math.random() * questions.length)
+
+  return (
+    <section style={{ marginBottom: rhythm(2) }}>
+      <h2>Random Wasabi Question</h2>
+      {questions[choice]}
     </section>
   )
 }
