0d3c9621e41797dc8a80fd29aabd0b567585f3e8
wasabigeek <wasabigeek@users.noreply.github.com>
2021-04-27 23:51:16 +0800

    refactor: Extract Question component

--- src/components/WasabiQuiz.js
+++ src/components/WasabiQuiz.js
@@ -2,60 +2,65 @@ import React from "react"
 
 import { rhythm } from "../utils/typography"
 
+const Question = ({ text, answers }) => {
+  return (
+    <div>
+      <p>{text}</p>
+      <ol>
+        {answers.map((answer) => <li>{answer}</li>)}
+      </ol>
+    </div>
+  );
+}
+
 const WasabiQuiz = () => {
   const questions = [
-    <React.Fragment>
-      <p>
-        Which plant belongs to the same family as wasabi?
-      </p>
-      <ol>
-        <li>Cabbage</li>
-        <li>Carrot</li>
-        <li>Potato</li>
-        <li>Onion</li>
-      </ol>
-    </React.Fragment>,
-    <React.Fragment>
-      <p>
-        How long does it take for wasabi to mature?
-      </p>
-      <ol>
-        <li>6 months</li>
-        <li>8 months</li>
-        <li>1 year</li>
-        <li>2 years</li>
-      </ol>
-    </React.Fragment>,
-    <React.Fragment>
-      <p>
-        What is the main component of wasabi substitutes?
-      </p>
-      <ol>
-        <li>Mustard</li>
-        <li>Horseradish</li>
-        <li>Green Chilli</li>
-        <li>White Peppercorns</li>
-      </ol>
-    </React.Fragment>,
-    <React.Fragment>
-      <p>
-        Which part of the wasabi plant is used to create the sushi condiment?
-      </p>
-      <ol>
-        <li>Stem</li>
-        <li>Root</li>
-        <li>Leaves</li>
-        <li>Fruit</li>
-      </ol>
-    </React.Fragment>,
+    {
+      text: "Which plant belongs to the same family as wasabi?",
+      answers: [
+        "Cabbage",
+        "Carrot",
+        "Potato",
+        "Onion",
+      ]
+    },
+    {
+      text: "How long does it take for wasabi to mature?",
+      answers: [
+        "6 months",
+        "8 months",
+        "1 year",
+        "2 years",
+      ]
+    },
+    {
+      text: "What is the main component of wasabi substitutes?",
+      answers: [
+        "Mustard",
+        "Horseradish",
+        "Green Chilli",
+        "White Peppercorns",
+      ]
+    },
+    {
+      text: "Which part of the wasabi plant is used to create the sushi condiment?",
+      answers: [
+        "Stem",
+        "Root",
+        "Leaves",
+        "Fruit",
+      ]
+    }
   ]
   const choice = Math.floor(Math.random() * questions.length)
 
-  return (
-    <section style={{ marginBottom: rhythm(2) }}>
-      <h2>Random Wasabi Question</h2>
-      {questions[choice]}
-    </section>
+  return (<section style={{ marginBottom: rhythm(2) }}>
+    <h2>Random Wasabi Question</h2>
+    <Question
+      text={questions[choice].text}
+      answers={questions[choice].answers}
+    />
+  </section>
   )
 }
 
