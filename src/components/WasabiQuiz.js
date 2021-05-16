f57e5c094992efdfbe7e549f2e4c02cb984dc1c8
wasabigeek <wasabigeek@users.noreply.github.com>
2021-05-16 15:17:00 +0800

    feat: Add next question button to quiz

--- src/components/WasabiQuiz.js
+++ src/components/WasabiQuiz.js
@@ -2,75 +2,83 @@ import React, { useState } from "react"
 
 import { rhythm } from "../utils/typography"
 
-const Question = ({ text, answers, correctAnswer }) => {
+const Question = ({ text, answers, correctAnswer, progressQuestion }) => {
   const [showAnswer, setShowAnswer] = useState(false);
+
+  const triggerNextQuestion = () => {
+    setShowAnswer(false);
+    progressQuestion();
+  }
 
   return (
     <div>
       <p>{text}</p>
-      <ol>
+      <ul>
         {answers.map(answer => (
-          <li style={showAnswer && answer === correctAnswer ? { color: "green" } : {}}>
+          <li style={showAnswer && answer === correctAnswer ? { color: "green", fontWeight: "bold" } : {}}>
             {answer}
           </li>
         ))}
-      </ol>
+      </ul>
       <div>
-        <button onClick={() => setShowAnswer(true)}>Show Answer</button>
+        <button style={{ marginRight: 10 }} onClick={() => setShowAnswer(true)}>Show Answer</button>
+        <button onClick={triggerNextQuestion}>Next Question</button>
       </div>
     </div>
   );
 }
 
-const WasabiQuiz = () => {
-  const questions = [
-    {
-      text: "Which plant belongs to the same family as wasabi?",
-      answers: [
-        "Carrot",
-        "Potato",
-        "Cabbage",
-        "Onion",
-      ],
-      correctAnswer: "Cabbage"
-    },
-    {
-      text: "How long does it take for wasabi to mature?",
-      answers: [
-        "6 months",
-        "8 months",
-        "1 year",
-        "2 years",
-      ],
-      correctAnswer: "2 years"
-    },
-    {
-      text: "What is the main component of wasabi substitutes?",
-      answers: [
-        "Mustard",
-        "Horseradish",
-        "Green Chilli",
-        "White Peppercorns",
-      ],
-      correctAnswer: "Horseradish"
-    },
-    {
-      text: "Which part of the wasabi plant is used to create the sushi condiment?",
-      answers: [
-        "Stem",
-        "Root",
-        "Leaves",
-        "Fruit",
-      ],
-      correctAnswer: "Stem"
-    }
-  ]
-  const choice = Math.floor(Math.random() * questions.length)
+const QUESTIONS = [
+  {
+    text: "Which plant belongs to the same family as wasabi?",
+    answers: [
+      "Carrot",
+      "Potato",
+      "Cabbage",
+      "Onion",
+    ],
+    correctAnswer: "Cabbage"
+  },
+  {
+    text: "How long does it take for wasabi to mature?",
+    answers: [
+      "6 months",
+      "8 months",
+      "1 year",
+      "2 years",
+    ],
+    correctAnswer: "2 years"
+  },
+  {
+    text: "What is the main component of wasabi substitutes?",
+    answers: [
+      "Mustard",
+      "Horseradish",
+      "Green Chilli",
+      "White Peppercorns",
+    ],
+    correctAnswer: "Horseradish"
+  },
+  {
+    text: "Which part of the wasabi plant is used to create the sushi condiment?",
+    answers: [
+      "Stem",
+      "Root",
+      "Leaves",
+      "Fruit",
+    ],
+    correctAnswer: "Stem"
+  }
+]
+
+const WasabiQuiz = ({ questions = QUESTIONS }) => {
+  const [questionIndex, setQuestionIndex] = useState(Math.floor(Math.random() * questions.length));
 
   return (<section style={{ marginBottom: rhythm(2) }}>
     <h2>Random Wasabi Question</h2>
     <Question
-      {...questions[choice]}
+      {...questions[questionIndex]}
+      progressQuestion={() => setQuestionIndex((questionIndex + 1) % questions.length)}
     />
   </section>
   )
