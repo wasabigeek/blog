5dadc0dd2105efca01f242dc30827c74c5fcae2c
Nicholas Koh <nicholas.koh@shopify.com>
2022-02-15 21:11:02 +0800

    Improve obsidian/syncer

--- obsidian/syncer.rb
+++ obsidian/syncer.rb
@@ -1,24 +1,33 @@
+require "active_support/inflector/methods"
+require "active_support/inflector/transliterate"
 require "fileutils"
 
-# OBSIDIAN_DIRECTORY = ENV['OBSIDIAN_DIRECTORY']
+def parameterize(word)
+  ActiveSupport::Inflector.parameterize(word, separator: '-')
+end
+
+OBSIDIAN_DIRECTORY = ENV['OBSIDIAN_DIRECTORY']
 
 # source_file = ARGV.first
-source_file = "Blog/My Learning Workflow for 2022.md"
+post_title = "Reading a Ruby gem with VSCode"
+source_file = "Blog/Published/#{post_title}.md"
 
 article_path = File.join(OBSIDIAN_DIRECTORY, source_file)
 original_md = File.open(article_path, "r").read
 
 FILE_REGEX = /\!\[\[(?<file>.+)\]\]/
 files_matchdata = original_md.scan(FILE_REGEX)
+directory = "../content/blog/#{parameterize(post_title)}/"
+FileUtils.mkdir_p(directory)
 # copy files
 files_matchdata.flatten.each do |filename|
   filepath = File.join(OBSIDIAN_DIRECTORY, "Files", filename)
-  FileUtils.copy(filepath, "./content/blog/my-learning-workflow-for-2022/#{filename}")
+  FileUtils.copy(filepath, "#{directory}#{filename}")
 end
 
 new_md = original_md
   .gsub(/(?<!\!)\[\[(?<linktitle>.+)\]\]/, '\k<linktitle>')
   .gsub(FILE_REGEX, '![\k<file>](./\k<file>)')
-File.open("./content/blog/my-learning-workflow-for-2022/index.md", "w+") do |file|
+File.open("#{directory}/index.md", "w+") do |file|
   file.write(new_md)
 end
