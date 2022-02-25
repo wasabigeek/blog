6568cf7fb5ce37028b1a868ad6987977343e9412
wasabigeek <wasabigeek@users.noreply.github.com>
2022-02-25 22:19:02 +0800

    Add naive parsing of frontmatter

--- obsidian/obsidian_blog_post.rb
+++ obsidian/obsidian_blog_post.rb
@@ -13,6 +13,18 @@ class ObsidianBlogPost
     @original_markdown.scan(FILE_REGEX).flatten
   end
 
+  def description
+    frontmatter_hash[:description]
+  end
+
+  def tags
+    frontmatter_hash[:tags]
+  end
+
+  def title
+    frontmatter_hash[:title]
+  end
+
   # TODO: extract - this depends on the Blog's format
   def to_blog_markdown
     @original_markdown
@@ -20,5 +32,34 @@ class ObsidianBlogPost
       .first
       .gsub(/(?<!\!)\[\[(?<linktitle>.+)\]\]/, '\k<linktitle>')
       .gsub(FILE_REGEX, '![\k<file>](./\k<file>)')
+  end
+
+  private
+
+  def frontmatter_hash
+    return @frontmatter_hash unless @frontmatter_hash.nil?
+
+    raw_frontmatter = @original_markdown
+                      .match(/---\n(?<raw_frontmatter>\X+)\n---\n/)
+                      .named_captures['raw_frontmatter']
+
+    @frontmatter_hash = raw_frontmatter
+      .split("\n")
+      .to_h do |item|
+        key, value = item.split(': ').map(&:chomp)
+        transformed_value = case key
+                            when 'published'
+                              value == 'true'
+                            when 'tags'
+                              value.scan(/["'](\w+)["']/).flatten
+                            else
+                              value.gsub('"', '')
+                            end
+
+        [
+          key.to_sym,
+          transformed_value
+        ]
+      end
   end
 end
